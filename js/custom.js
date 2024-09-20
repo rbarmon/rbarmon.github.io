83aadc108f5aac8a080ca8a9223b7964401b6180
Rian Barrett <rbar0032@student.monash.edu>
2024-09-20 15:14:15 +1000

    adding en and ja mode, additional styling and more content

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,33 @@
 
   "use strict";
 
-  $('body').addClass('dark-mode');
-    $('.work_container .content').addClass('dark-mode');
-    $('.work_container .date').addClass('dark-mode');
+  // $('body').addClass('dark-mode');
+  //   $('.work_container .content').addClass('dark-mode');
+  //   $('.work_container .date').addClass('dark-mode');
+        document.getElementById('toggle-language').addEventListener('click', function () {
+            const langToggleButton = this;
+            const isEnglish = langToggleButton.textContent === "日本語";
+    
+            // Toggle button text
+            langToggleButton.textContent = isEnglish ? "English" : "日本語";
+    
+            // Switch language for all elements with `data-en` and `data-ja`
+            document.querySelectorAll('[data-en]').forEach(function (el) {
+                const enText = el.getAttribute('data-en');
+                const jaText = el.getAttribute('data-ja');
+                el.textContent = isEnglish ? jaText : enText;
+            });
 
+            // Switch language for `data-hover` attributes
+            document.querySelectorAll('[data-hover]').forEach(function (element) {
+              const enText = element.getAttribute('data-en');
+              const jaText = element.getAttribute('data-ja');
+              const hoverText = isEnglish ? jaText : enText;
+              element.setAttribute('data-hover', hoverText); // Set hover text
+          });
+
+
+        });
     // COLOR MODE
     $('.color-mode').click(function(){
         $('.color-mode-icon').toggleClass('active')
